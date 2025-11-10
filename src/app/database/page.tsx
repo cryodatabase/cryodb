0a55e83a5306d3b930855a5e35b68a2a70e18dbd
cryorepository <cryorepository@protonmail.com>
2025-11-10 15:23:02 +1100

    test

--- src/app/database/page.tsx
+++ src/app/database/page.tsx
@@ -66,7 +66,6 @@ export default async function DatabasePage({ searchParams }: { searchParams: Pro
           .map((result, index) => (
             <Link
               key={index}
-              // href={`/database/${encodeURIComponent(result.preferred_name.toLowerCase())}`}
               href={`/database/${encodeURIComponent(result.id.toLowerCase())}`}
               className="group flex flex-col py-4"
             >
